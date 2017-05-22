54afa36f434c2f21c60d268a006c16919a654019
Jeffrey Cooper <jcooper37@cnm.edu>
2017-05-22 16:55:00 -0600

    comment out lines for development

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app/app.module";
-import { enableProdMode } from "@angular/core";
+// comment out the following line until you go live. then activate it!
+// import { enableProdMode } from "@angular/core";
 
 //only use when app is going live effects debugging
 // during testing, comment the following line out, then return it to the script when going live
-enableProdMode();
+// enableProdMode();
 // "bootstrap" below (bootstrapModule) means start Angular from nothing. It's the beginning of running Angular
 platformBrowserDynamic().bootstrapModule(AppModule);
